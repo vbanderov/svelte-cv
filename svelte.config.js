499e52ebfd6fb06e08de19bc19461e0e653f8280
Vladimir Banderov <like.a.flower.in.a.basement@gmail.com>
2022-12-19 16:54:52 -0700

    set base in config

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -12,10 +14,13 @@ const config = {
 			// default options are shown
 			pages: 'build',
 			assets: 'build',
-			precompress: false,
+			precompress: false
 		}),
-		appDir: 'app'
-	},
+		appDir: 'app',
+		paths: {
+			base: dev ? '' : '/svelte-sv'
+		}
+	}
 };
 
 export default config;
