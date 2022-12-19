21170375caebd7e3a0bb9bd0cf8cf4c94cff49c5
Vladimir Banderov <like.a.flower.in.a.basement@gmail.com>
2022-12-19 14:09:56 -0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import avatar from './assets/avatar.jpeg'
+</script>
+
+<main>
+    <div class="text-2xl">Vladimir Banderov</div>
+  <div>
+
+      <img src={avatar} class="rounded-full" alt="avatar" />
+  </div>
+
+</main>
