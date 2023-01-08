f3d32870bd3bd938e7cd1e1985c325bf870cadee
Vladimir Banderov <like.a.flower.in.a.basement@gmail.com>
2023-01-08 16:21:46 -0700

    remove dotted line on print

--- src/lib/components/section.svelte
+++ src/lib/components/section.svelte
@@ -4,7 +4,7 @@
 
 <div class="pt-2 dark:text-white">
 	<div
-		class="text-bold mt-4 mb-2 border-b-2 border-dotted border-blue-600 pb-1 text-xl font-bold uppercase text-blue-800 dark:border-orange-300 dark:text-orange-300"
+		class="text-bold mt-4 mb-2 border-b-2 border-dotted border-blue-600 pb-1 text-xl font-bold uppercase text-blue-800 dark:border-orange-300 dark:text-orange-300 print:border-none print:pb-0"
 	>
 		{title}
 	</div>
